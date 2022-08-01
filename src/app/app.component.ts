3ddf3fc6269d62fbd22562bce9a6f988b1ebcb9c
Angelino Valeta <angelinovaleta@gmail.com>
2022-08-01 12:14:55 +0100

    Stepper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, Validators, FormGroup} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'bs-stepper';
+
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  isLinear = false;
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+  
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
+  }
+
 }
